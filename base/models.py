824fd828facbefb4436f41fcb5c31cef57d18b9b
redihoxha <redi.hoxha@y42.com>
2023-07-20 13:23:47 +0200

    Initial commit, environment setup, configuring Views&URL and doing some sample templates.

---
+++ base/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Room(models.Model):
+    #host =
+    #topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
