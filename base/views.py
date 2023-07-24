f37a06a4e528f2849726c13b4fd5b8957d050093
redihoxha <redi.hoxha@y42.com>
2023-07-24 15:34:04 +0200

    Create a form for creating, deleting and updating a room. Add a search bar with multiple filtering and fix a bit the HTML files.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
+from django.db.models import Q
+
 
 # rooms = [
 #     {'id': 1, 'name': 'Learn React'},
@@ -10,8 +13,16 @@ from .models import Room
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    topic_count = topics.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'topic_count': topic_count}
     return render(request, 'base/home.html', context)
 
 
@@ -19,3 +30,34 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
