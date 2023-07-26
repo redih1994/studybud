20c114cfbaad33e72e0c120f404581995d92881c
redihoxha <redi.hoxha@y42.com>
2023-07-26 16:10:12 +0200

    Add the styling for the web interface and install a theme.

--- base/views.py
+++ base/views.py
@@ -71,7 +71,9 @@ def home(request):
     room_count = rooms.count()
     topics = Topic.objects.all()
     topic_count = topics.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'topic_count': topic_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count,
+               'topic_count': topic_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
@@ -91,15 +93,30 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topics_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topics_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -107,16 +124,19 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You do not have permissions to update the room.')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topics_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topics_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 
